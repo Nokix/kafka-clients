012cf4121503bf7edb48bdd8583f2dfd538c949b
Oliver Kohl <inhaber@conteco.gmbh>
2022-11-06 18:27:15 +0100

    Lombok

--- src/main/java/gmbh/conteco/tweets/Tweet.java
+++ src/main/java/gmbh/conteco/tweets/Tweet.java
@@ -1,46 +1,12 @@
 package gmbh.conteco.tweets;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Tweet {
-    public Tweet(Long id, String language, String text) {
-        this.id = id;
-        this.language = language;
-        this.text = text;
-    }
-
     private Long id;
-
     private String language;
     private String text;
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getLanguage() {
-        return language;
-    }
-
-    public void setLanguage(String language) {
-        this.language = language;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    @Override
-    public String toString() {
-        return "Tweet{" +
-                "id=" + id +
-                ", language='" + language + '\'' +
-                ", text='" + text + '\'' +
-                '}';
-    }
 }
